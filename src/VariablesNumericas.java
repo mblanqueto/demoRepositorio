e1e1374bf346e43cd8ec580b4aa198e41e14a489
mblanqueto <melissa@uqroo.edu.mx>
2021-09-08 13:45:14 -0500

    Creación del proyecto

---
+++ src/VariablesNumericas.java
@@ -0,0 +1,5 @@
+public class VariablesNumericas {
+    public static void main(String[] args) {
+        System.out.println("Programa que demuestra la declaracion y uso de variables numericas");
+    }
+}
